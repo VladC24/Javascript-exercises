c8ddb9a57714c7685e6def67b7f57898f688d0b4
VladC <vlad.chiriac24@gmail.com>
2019-08-08 10:13:24 +0100

    Add Book class

--- books_and_stuff.js
+++ books_and_stuff.js
@@ -28,3 +28,17 @@ class Media {
     this.ratings.push(value);
   }
 }
+
+class Book extends Media {
+  constructor(author, title, pages){
+    this._author = author;
+    super(title);
+    this._pages = pages;
+  }
+  get author(){
+    return this._author;
+  }
+  get pages(){
+    return this._pages;
+  }
+}
